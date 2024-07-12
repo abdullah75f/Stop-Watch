19ca6c32a11e66e9d62148d99a1eb15fc518ac8f
zaz7 <abdullah75farid@gmail.com>
2024-07-13 00:58:11 +0300

    final commit

--- project-4.js
+++ project-4.js
@@ -77,3 +77,11 @@ startStopBtn.addEventListener("click", function(){
     }
 
 });
+
+resetBtn.addEventListener("click", function(){
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("timer").innerHTML=("00:00:00");
+});
