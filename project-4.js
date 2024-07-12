a284fd26b152555e94e05b4b5b8ff278897d7ebc
zaz7 <abdullah75farid@gmail.com>
2024-07-13 00:13:25 +0300

    logic

--- project-4.js
+++ project-4.js
@@ -6,6 +6,28 @@ const resetBtn = document.querySelector("#resetBtn");
 
 //Varbiales for time values
 
-let second = 0 ;
+let seconds = 0 ;
 let minutes = 0;
 let hours = 0 ;
+
+//stop watch logic/function
+
+function stopWatch(){
+    seconds++;
+
+    if(seconds / 60 ===1){
+
+    }
+        seconds = 0;
+        minutes ++;
+        
+        if(minutes/60 ===1){
+            minutes=0;
+            hours++;
+        }
+
+        
+
+     
+
+}
