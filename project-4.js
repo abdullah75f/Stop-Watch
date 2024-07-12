df17db4dbe7816f3f146c37f38c74a787b22f094
zaz7 <abdullah75farid@gmail.com>
2024-07-13 00:07:12 +0300

    times

--- project-4.js
+++ project-4.js
@@ -5,3 +5,7 @@ const startStopBtn = document.querySelector("#startStopBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
 //Varbiales for time values
+
+let second = 0 ;
+let minutes = 0;
+let hours = 0 ;
